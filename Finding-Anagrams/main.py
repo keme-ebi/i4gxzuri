52b3f9314d64999db2d3c97a0dc5405a0a2cfd06
keme-ebi <kelvin6bolou@gmail.com>
2022-05-25 14:08:49 +0100

    first commit

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -1,11 +1,18 @@
-# Check if a word is an anagrams 
+# Check if a word is an anagrams
 # Example:
 # find_anagrams("hello") --> False
 # find_anagrams("racecar") --> True
 
 
-def find_anagrams(word):
+def find_anagrams(first, second):
     # [assignment] Add your code here
-
+    if (sorted(first) == sorted(second)):
+        print("True")
+    else:
+        print("False")
     return True
 
+
+first = "plead"
+second = "paled"
+find_anagrams(first, second)
