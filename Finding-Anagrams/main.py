9ca483c5b6a39ac3debd81f78908c740cea9d355
keme-ebi <105162424+keme-ebi@users.noreply.github.com>
2022-06-05 00:16:29 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -6,6 +6,9 @@
 
 def find_anagrams(first, second):
     # [assignment] Add your code here
+    first = input("input first word: ")
+    second = input("input second word: ")
+    
     if (sorted(first) == sorted(second)):
         print("True")
     else:
